35b81c1b51c2a57dd9f8daf90ec80c11b135bf46
RBopalk1 <rajat.bopalkar1@t-mobile.com>
2023-04-06 18:41:34 +0530

    codeChanges_undoActions

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -6,12 +6,18 @@ import {
   getAllBooks,
   ReadingListBook,
   searchBooks,
-  getBooksError
+  getBooksError,
+  removeFromReadingList
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
+import { 
+  MatSnackBar, 
+  MatSnackBarHorizontalPosition, 
+  MatSnackBarVerticalPosition 
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'tmo-book-search',
@@ -24,14 +30,16 @@ export class BookSearchComponent implements OnInit, OnDestroy {
   public errorMessage = '';
   public readonly getBooks$ = this.store.pipe(select(getAllBooks));
   public readonly getBooksError$ = this.store.pipe(select(getBooksError));
-
+  private hPosition: MatSnackBarHorizontalPosition = 'center';
+  private vPosition: MatSnackBarVerticalPosition = 'bottom';
   searchForm = this.fb.group({
     term: ''
   });
 
   constructor(
     private readonly store: Store,
-    private readonly fb: FormBuilder
+    private readonly fb: FormBuilder,
+    private snackbar: MatSnackBar
   ) {}
 
   get searchTerm(): string {
@@ -58,14 +66,18 @@ export class BookSearchComponent implements OnInit, OnDestroy {
       });
   }
 
-  // formatDate(date: void | string) {
-  //   return date
-  //     ? new Intl.DateTimeFormat('en-US').format(new Date(date))
-  //     : undefined;
-  // }
-
   addBookToReadingList(book: Book) {
     this.store.dispatch(addToReadingList({ book }));
+  const addedSnackbar = this.snackbar.open(
+    'Added to reading list', 'Undo', {
+      duration: 3000,
+      horizontalPosition: this.hPosition,
+      verticalPosition: this.vPosition
+    });
+    addedSnackbar.onAction().subscribe(() => {
+      const item:any = {books: book.id};
+    this.store.dispatch(removeFromReadingList({item}));
+    })
   }
 
   searchExample() {
